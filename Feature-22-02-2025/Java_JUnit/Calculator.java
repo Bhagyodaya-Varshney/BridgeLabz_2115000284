a3c46f5dd876aa41963ed1978480e9f8beac1323
Bhagyodaya_Varshney <mtrbhagyodayame@email.com>
2025-02-24 20:01:04 +0530

    Assignment 21 Commit

--- Feature-22-02-2025/Java_JUnit/Calculator.java
+++ Feature-22-02-2025/Java_JUnit/Calculator.java
@@ -1,8 +1,11 @@
 package Java_JUnit;
+//
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+//
 public class Calculator {
     public int add(int a, int b) { return a + b; }
     public int subtract(int a, int b) { return a - b; }
@@ -13,30 +16,43 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        System.out.println(calc.add(5, 3));
-        System.out.println(calc.subtract(10, 4));
-        System.out.println(calc.multiply(6, 2));
-        System.out.println(calc.divide(9, 3));
+//        Calculator calc = new Calculator();
+//        System.out.println(calc.add(5, 3));
+//        System.out.println(calc.subtract(10, 4));
+//        System.out.println(calc.multiply(6, 2));
+//        System.out.println(calc.divide(9, 3));
+        System.out.println("Hello World!");
     }
 }
+//class CalculatorTest {
+//    Calculator calc = new Calculator();
+//
+//    @org.junit.jupiter.api.Test
+//    void testAdd() {
+//        int res = calc.add(2, 3);
+//        org.junit.jupiter.api.Assertions.assertEquals(6,res);
+//    }
+//
+////    @Test
+////    void testSubtract() { assertEquals(1, calc.subtract(4, 3)); }
+////
+////    @Test
+////    void testMultiply() { assertEquals(6, calc.multiply(2, 3)); }
+////
+////    @Test
+////    void testDivide() { assertEquals(2, calc.divide(6, 3)); }
+////
+////    @Test
+////    void testDivideByZero() {
+////        assertThrows(ArithmeticException.class, () -> calc.divide(5, 0));
+////    }
+//}
+
 class CalculatorTest {
-    Calculator calc = new Calculator();
-
     @Test
-    void testAdd() { assertEquals(5, calc.add(2, 3)); }
-
-    @Test
-    void testSubtract() { assertEquals(1, calc.subtract(4, 3)); }
-
-    @Test
-    void testMultiply() { assertEquals(6, calc.multiply(2, 3)); }
-
-    @Test
-    void testDivide() { assertEquals(2, calc.divide(6, 3)); }
-
-    @Test
-    void testDivideByZero() {
-        assertThrows(ArithmeticException.class, () -> calc.divide(5, 0));
+    void testAddition() {
+        Calculator calc = new Calculator();
+        assertEquals(10, calc.add(5, 5), "Addition should be correct");
     }
 }
+
